2ad82a0f178fb51dc0d94a15fe48f872f1167205
Johnlyn del Castillo <johnlyndelcastillo@gmail.com>
2025-08-02 19:15:19 +0800

    removed unused props in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,10 @@ export default {
     extend: {
       colors: {
         'main-background': '#FFF6E3',
-        'background-left': '#FFD2A0 ',
-        'background-top': '#CDC1FF',
         'cv-button': '#FEEE91',
         'facebook': '#81BFDA',
         'github': '#F6C794',
         'gmail': '#FF8383',
-        'about-bg': '#FFF9BF',
         'card-bg': '#DCCFC0'
 
 
